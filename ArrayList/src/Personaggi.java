dbe2fc6df3cc7d9ec01fc2c2c42e27966e094aa0
vol4ainf-05 <vol4ainf-05@vol05.colamonico.itc>
2022-02-01 12:01:54 +0100

    primo commit

---
+++ ArrayList/src/Personaggi.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Personaggi {
+
+	public static void main(String[] args) {
+		
+        class Main {
+       	public void main(String[] args) {
+		ArrayList<String> personaggio = new ArrayList<String>();
+		
+		personaggio.add("Tobey");
+		personaggio.add("Andrew");
+		personaggio.add("Alfred");
+		personaggio.add("Benedict");
+		
+		System.out.println(personaggio);
+       	}
+  }
+}
+}
